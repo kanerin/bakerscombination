6699a4cfefd9c7bab873dc8c91eab8ad6beeae44
kanerin <kanerin1004@gmail.com>
2022-02-02 00:10:17 +0900

    Design changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,12 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
         if params[:search_1] == nil
-            @bakerscombinations= Bakerscombination.all.order(created_at: :desc).page(params[:page_1]).per(5)
+            @bakerscombinations= Bakerscombination.where(user_id: current_user.id).order(created_at: :desc).page(params[:page_1]).per(5)
         elsif params[:search_1] == ''
-            @bakerscombinations= Bakerscombination.all.order(created_at: :desc).page(params[:page_1]).per(5)
+            @bakerscombinations= Bakerscombination.where(user_id: current_user.id).order(created_at: :desc).page(params[:page_1]).per(5)
         else
         #部分検索
-            @bakerscombinations = Bakerscombination.where("name LIKE ? ",'%' + params[:search_1] + '%').order(created_at: :desc).page(params[:page_1]).per(5)
+            @bakerscombinations = Bakerscombination.where(user_id: current_user.id).where("name LIKE ? ",'%' + params[:search_1] + '%').order(created_at: :desc).page(params[:page_1]).per(5)
         end
 
         @items = @user.bakerscombinations
