320df2cc4cbf435a1ba46512754c5089f671a9f3
kanerin <kanerin1004@gmail.com>
2022-02-02 17:02:23 +0900

    Change design & my page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,16 +3,16 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
         if params[:search_1] == nil
-            @bakerscombinations= Bakerscombination.where(user_id: current_user.id).order(created_at: :desc).page(params[:page_1]).per(5)
+            @bakerscombinations= Bakerscombination.where(user_id: params[:id]).order(created_at: :desc).page(params[:page_1]).per(5)
         elsif params[:search_1] == ''
-            @bakerscombinations= Bakerscombination.where(user_id: current_user.id).order(created_at: :desc).page(params[:page_1]).per(5)
+            @bakerscombinations= Bakerscombination.where(user_id: params[:id]).order(created_at: :desc).page(params[:page_1]).per(5)
         else
         #部分検索
-            @bakerscombinations = Bakerscombination.where(user_id: current_user.id).where("name LIKE ? ",'%' + params[:search_1] + '%').order(created_at: :desc).page(params[:page_1]).per(5)
+            @bakerscombinations = Bakerscombination.where(user_id: params[:id]).where("name LIKE ? ",'%' + params[:search_1] + '%').order(created_at: :desc).page(params[:page_1]).per(5)
         end
-
+        
         @items = @user.bakerscombinations
-        likes = Like.where(user_id: current_user.id).pluck(:bakerscombination_id)
+        likes = Like.where(user_id: params[:id]).pluck(:bakerscombination_id)
         
         if params[:search_2] == nil
             @likes_list_array = Bakerscombination.order(created_at: :desc).find(likes)
